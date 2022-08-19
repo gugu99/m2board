6c2d1667ca965fe335770e5ee85c8e5aec9fc542
daeun <daeun@localhost>
2022-08-19 12:33:11 +0900

    로그인 상태 검증 추가

--- m2board/src/main/java/controller/BoardListController.java
+++ m2board/src/main/java/controller/BoardListController.java
@@ -8,6 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import service.BoardService;
 import service.IBoardService;
@@ -18,6 +19,15 @@ public class BoardListController extends HttpServlet {
 	private IBoardService boardService;
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		
+		System.out.println("session --- " + session.getAttribute("loginMember"));
+		
+		if (session.getAttribute("loginMember") == null) { // 로그인안된 상태
+			response.sendRedirect(request.getContextPath() + "/index");
+			return;
+		}
+		
 		// 컨트롤러
 		// 1) 요청 받아 분석
 		int rowPerPage = 10;
--- m2board/src/main/java/controller/BoardOneController.java
+++ m2board/src/main/java/controller/BoardOneController.java
@@ -8,6 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import service.BoardService;
 import service.IBoardService;
@@ -19,6 +20,15 @@ public class BoardOneController extends HttpServlet {
 	private IBoardService boardService;
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		HttpSession session = request.getSession();	
+		
+		System.out.println("session --- " + session.getAttribute("loginMember"));
+		
+		if (session.getAttribute("loginMember") == null) { // 로그인안된 상태
+			response.sendRedirect(request.getContextPath() + "/index");
+			return;
+		}
 		
 		int boardNo = Integer.parseInt(request.getParameter("boardNo"));
 		
